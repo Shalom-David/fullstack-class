0b3ba2f370be8238108c056aceef6fed0230df81
Shalom David <shalom097@gmail.com>
2022-07-25 20:30:35 +0300

    edit

--- file1.js
+++ file1.js
@@ -1,6 +1,1 @@
-<<<<<<< HEAD
-console.log('hello World')
-=======
-console.log('hello World')
-console.log('hello israel');
->>>>>>> 3e9047e0bb8684d70626400bb2c24dc7667e8af3
+console.log('hello world')
